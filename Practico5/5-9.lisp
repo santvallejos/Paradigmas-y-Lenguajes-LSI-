62ef8b8a4a30ed5b59eab589a5cabce007813d84
santvallejos <vallejossantiago1412@gmail.com>
2025-06-09 23:34:22 -0300

    Activity 5.9

---
+++ Practico5/5-9.lisp
@@ -0,0 +1,12 @@
+#| 
+    Definir la función sumo-ambos, la que a partir de 2 Listas no vacías: LISTA1 y
+    LISTA2, devuelva una nueva Lista con el resultado de sumar elemento a elemento, los elementos
+    de la misma posición. (1° elemento de LISTA1 con el 1° elemento de LISTA”, 2° elemento de
+    LISTA1 con el 2° elemento de LISTA”, ….)
+|#
+
+(defun sumo-ambos (lista1 lista2)
+    (mapcar (lambda (x y)
+        (if (and (numberp x) (numberp y)) (+ x y)));Funcion principal
+    lista1 lista2);parametros de recorrido
+);defun
